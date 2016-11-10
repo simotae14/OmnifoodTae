be6cba2ce2ec1f308edb6ff56277ae3bfd07d9df
Simone Taeggi <startae14@gmail.com>
2016-11-10 17:12:42 +0100

    aggiunto menu mobile

--- resources/js/changeLang.js
+++ resources/js/changeLang.js
@@ -1,21 +1,20 @@
-var  attivaIT = document.querySelector(".active-IT");
-var  attivaEN = document.querySelector(".active-EN");
 var spanITA = document.querySelectorAll(".ita");
 var spanEN = document.querySelectorAll(".eng");
 
-attivaIT.addEventListener("click", function () {
+function attivaITA(){
   spanITA.forEach(function(el) {
       el.style.display = 'inline';
   });
   spanEN.forEach(function(el) {
       el.style.display = 'none';
   });
-});
-attivaEN.addEventListener("click",function(){
+}
+
+function attivaENG(){
   spanEN.forEach(function(el) {
       el.style.display = 'inline';
   });
   spanITA.forEach(function(el) {
       el.style.display = 'none';
   });
-});
+}
