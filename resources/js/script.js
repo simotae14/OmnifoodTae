93af70dc06e37a7d63debf56b61cd62e4d560a03
Simone Taeggi <startae14@gmail.com>
2016-11-13 00:23:33 +0100

    Scrolling to elements
    aggiunto effetto di scrolling smooth ai vari elementi

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+  
+  /* Per la sticky navigation */
+  
   $('.js--section-features').waypoint(function (direction) {
     if (direction === "down") {
       $('nav').addClass('sticky');
@@ -7,5 +10,30 @@ $(document).ready(function () {
     }
   }, {
     offset: '60px'
+  });
+  
+  /* Per lo scroll dei bottoni */
+  $('.js--scroll-to-plans').click(function () {
+    $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+  });
+  /* Per lo scroll dei bottoni */
+  $('.js--scroll-to-start').click(function () {
+    $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+  });
+  
+  /* Scroll Navigazione */
+  $(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+        return false;
+        }
+      }
+    });
   });
 });
