984deed5be3e0369396f272c7e91a6cf3f86c87c
Simone Taeggi <startae14@gmail.com>
2016-11-13 18:00:49 +0100

    OTTIMIZZAZIONE immagini e codice minimizzato
    ottimizzate le dimensioni delle immagini e create le versioni
    minimizzate dei cSS e dei js

---
+++ resources/js/changeLang-min.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".active-IT").click(function(){$(".ita").css("display","inline"),$(".eng").css("display","none")}),$(".active-EN").click(function(){$(".eng").css("display","inline"),$(".ita").css("display","none")})});
