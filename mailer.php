5dc5c0fb1d725db6fb5136d7c785bfff33d8103a
Simone Taeggi <startae14@gmail.com>
2016-11-18 11:59:50 +0000

    fix

--- mailer.php
+++ mailer.php
@@ -1,35 +1,41 @@
 <?php
 
     // Get the form fields, removes html tags and whitespace.
-    $name = strip_tags(trim($_POST["name"]));
-    $name = str_replace(array("\r","\n"),array(" "," "),$name);
-    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-    $message = trim($_POST["message"]);
+    //$name = strip_tags(trim($_POST["name"]));
+    //$name = str_replace(array("\r","\n"),array(" "," "),$name);
+    //$email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    //$message = trim($_POST["message"]);
 
     // Check the data.
-    if (empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        header("Location: https://taeomnifood.herokuapp.com/index.php?success=-1#form");
-        exit;
-    }
+    //if (empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    //    header("Location: https://taeomnifood.herokuapp.com/index.php?success=-1#form");
+    //    exit;
+    //}
 
     // Set the recipient email address. Update this to YOUR desired email address.
-    $recipient = "startae14@gmail.com";
+    //$recipient = "startae14@gmail.com";
 
     // Set the email subject.
-    $subject = "New contact from $name";
+    //$subject = "New contact from $name";
 
     // Build the email content.
-    $email_content = "Name: $name\n";
-    $email_content .= "Email: $email\n\n";
-    $email_content .= "Message:\n$message\n";
+    //$email_content = "Name: $name\n";
+    //$email_content .= "Email: $email\n\n";
+    //$email_content .= "Message:\n$message\n";
 
     // Build the email headers.
-    $email_headers = "From: $name <$email>";
+    //$email_headers = "From: $name <$email>";
 
     // Send the email.
-    mail($recipient, $subject, $email_content, $email_headers);
+    //mail($recipient, $subject, $email_content, $email_headers);
     
     // Redirect to the index.html page with success code
-    header("Location: https://taeomnifood.herokuapp.com/index.php?success=1#form");
+    //header("Location: https://taeomnifood.herokuapp.com/index.php?success=1#form");
+    
+    $mittente = 'From: "Il mio nome" <simone.taeggi@cerved.com> \r\n'; 
+    $destinatario = "simone.taeggi@cerved.com"; 
+    $oggetto = "email di prova"; 
+    $messaggio = "Ecco la mia prima email";  
+    mail($destinatario, $oggetto, $messaggio, $mittente); 
 
 ?>
