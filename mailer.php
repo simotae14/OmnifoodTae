2372b4bd8d493c23fb5a8c926d673a54700e144c
Simone Taeggi <startae14@gmail.com>
2016-11-18 11:54:30 +0000

    fix

--- mailer.php
+++ mailer.php
@@ -13,7 +13,7 @@
     }
 
     // Set the recipient email address. Update this to YOUR desired email address.
-    $recipient = "hello@webdesigncourse.co";
+    $recipient = "startae14@gmail.com";
 
     // Set the email subject.
     $subject = "New contact from $name";
